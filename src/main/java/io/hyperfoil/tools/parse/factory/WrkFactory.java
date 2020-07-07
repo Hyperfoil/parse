6910f6cfd7843406b6f7ded7f9696c26162de57f
John OHara <johara@redhat.com>
2020-07-07 18:13:53 +0100

    Updated wrkFactory to extract array of latency distributions from wrk2 output

--- src/main/java/io/hyperfoil/tools/parse/factory/WrkFactory.java
+++ src/main/java/io/hyperfoil/tools/parse/factory/WrkFactory.java
@@ -1,19 +1,16 @@
 package io.hyperfoil.tools.parse.factory;
 
-import io.hyperfoil.tools.parse.Eat;
-import io.hyperfoil.tools.parse.Exp;
-import io.hyperfoil.tools.parse.ExpRule;
-import io.hyperfoil.tools.parse.Parser;
+import io.hyperfoil.tools.parse.*;
 
 /**
  * Created by johara
- * WrkFactory - creates a Parser for wrk logs
+ * WrkFactory - creates a Parser for wrk/wrk2 logs
  */
 public class WrkFactory implements ParseFactory {
 
-    public Exp connections() { return new Exp("\\s+(?<threads>\\d*)\\sthreads and\\s*(?<connections>\\d*)\\s+connections"); }
+    public Exp connections() { return new Exp("connections", "\\s+(?<threads>\\d*)\\sthreads and\\s*(?<connections>\\d*)\\s+connections"); }
 
-    public Exp testConfig() { return new Exp("Running (?<duration>\\d*)(?<durationUnit>\\w*)\\stest @ (?<url>.*)"); }
+    public Exp testConfig() { return new Exp("config", "Running (?<duration>\\d*)(?<durationUnit>\\w*)\\stest @ (?<url>.*)"); }
 
     public Exp reqSec() {
         return new Exp("reqSec", "Requests/sec:\\s+(?<reqSec>\\d*\\.\\d*)");
@@ -21,19 +18,7 @@ public class WrkFactory implements ParseFactory {
 
     public Exp latency() { return new Exp("latency", "\\s*Latency\\s*(?<meanLatency>\\d*\\.\\d*)(?<meanLatencyUnit>\\w*)\\s*(?<stdDevLatency>\\d*\\.\\d*)(?<stdDevLatencyUnit>\\w*)\\s*(?<maxLatency>\\d*\\.\\d*)(?<maxLatencyUnit>\\w*)\\s*(?<stdDevPercent>\\d*\\.\\d*)"); }
 
-
-//                        " 50.000%  823.81ms\n" +
-//                                " 75.000%  908.29ms\n" +
-//                                " 90.000%  967.68ms\n" +
-//                                " 99.000%    1.02s \n" +
-//                                " 99.900%    1.03s \n" +
-//                                " 99.990%    1.04s \n" +
-//                                " 99.999%    1.04s \n" +
-//                                "100.000%    1.04s \n" +
-
-    public Exp LatencyDistributionHeader(){ return new Exp("latencyDistributionHeader", "\\s*Latency Distribution.*");
-    }
-    public Exp latencyDistribution() { return new Exp("latencyDistribution", "^\\s*(?<centile>\\d*\\.\\d*)%\\s*(?<latencyVal>\\d*\\.\\d*)(?<latencyUnit>\\w*)\\s*$"); }
+    public Exp latencyDistribution() { return new Exp("latencyDistribution", "^\\s*(?<centile>\\d*\\.\\d*)%\\s*(?<latencyVal>\\d*\\.\\d*)(?<latencyUnit>\\w*)\\s*.*"); }
 
     @Override
     public Parser newParser() {
@@ -48,11 +33,6 @@ public class WrkFactory implements ParseFactory {
         p.add(connections());
         p.add(latency());
         p.add(reqSec());
-        p.add(
-                LatencyDistributionHeader()
-                        .eat(Eat.Line)
-                        .addRule(ExpRule.Repeat)
-                        .add(latencyDistribution())
-        );
+        p.add(latencyDistribution().nest("latencyDistribution").setMerge(ExpMerge.AsEntry));
     }
 }
