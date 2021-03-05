cd43c96d222f848a1e91b25815bd16a6942414bd
Will Reichert <willr3@gmail.com>
2021-03-05 11:21:52 -0500

    update to released yaup and remove local test

--- src/test/java/io/hyperfoil/tools/parse/JsFunctionTest.java
+++ src/test/java/io/hyperfoil/tools/parse/JsFunctionTest.java
@@ -8,25 +8,4 @@ import java.util.Arrays;
 public class JsFunctionTest {
 
 
-    @Test
-    public void execute_multiline(){
-        JsStringFunction jsFunction = new JsStringFunction("  (path)=>{\n" +
-                "    const rtrn = new Json(true);\n" +
-                "    FileUtility.stream(path).forEach(line => {\n" +
-                "      if(line.trim() === \"\"){\n" +
-                "\n" +
-                "      } else if (line.match( /^\\d+$/ ) ) {\n" +
-                "        rtrn.add(new Json());\n" +
-                "        rtrn.getJson(rtrn.size() - 1).set(\"timestamp\", parseInt(line));\n" +
-                "      } else {\n" +
-                "        rtrn.getJson(rtrn.size() - 1).set(\"data\", Json.fromString(line));\n" +
-                "      }\n" +
-                "    });\n" +
-                "    return rtrn;\n" +
-                "  }"
-        );
-
-        Json response = jsFunction.apply("/home/wreicher/perfWork/mwperf/eap/2010/286/run/mwperf-server03.perf.lab.eng.rdu2.redhat.com/cli.datasource.log");
-        System.out.println(response);
-    }
 }
