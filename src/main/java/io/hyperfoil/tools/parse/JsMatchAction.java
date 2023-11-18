e376b2dc89524a3a9c7f4500e705aa773ad20e39
Will Reichert <willr3@gmail.com>
2023-11-17 22:06:41 -0500

    add match action code comments

--- src/main/java/io/hyperfoil/tools/parse/JsMatchAction.java
+++ src/main/java/io/hyperfoil/tools/parse/JsMatchAction.java
@@ -2,6 +2,19 @@ package io.hyperfoil.tools.parse;
 
 import io.hyperfoil.tools.yaup.json.Json;
 
+/**
+ * An action that runs javascript after an Exp matches the input line. The javascript function receives 4 parameters: line, match, pattern, parser
+ * line - the input string that matched
+ * match - The json that contains the matched values
+ * pattern - the Exp that matched the line
+ * parser - the Parser that ran the Exp.
+ *
+ * The CSV parser could use a JsMatchAction to create a custom Exp based on the name of the columns in the first row of the input:
+ * (line,match,exp,parser)=>{
+ *     parser.addAhead(new Exp('first',match.map(key=>`(?<${key}[^,]+)`).join(',')))
+ * }
+ * Using addAhead ensures the new Exp will run before the Exp that matched the header row.
+ */
 public class JsMatchAction extends JsFunction implements MatchAction {
 
    public JsMatchAction(String js){
--- src/main/java/io/hyperfoil/tools/parse/MatchAction.java
+++ src/main/java/io/hyperfoil/tools/parse/MatchAction.java
@@ -3,6 +3,7 @@ package io.hyperfoil.tools.parse;
 import io.hyperfoil.tools.yaup.json.Json;
 
 /**
+ * An action that runs after an Exp matches part of a line. Implementations are either Java or make use of JsMatchAction to define actions in yaml.
  *
  */
 public interface MatchAction {
